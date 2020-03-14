871c16c872bcfcb76bf3e78c01f607996b997fbc
Caleb Rudnicki <caleb.rudnicki@nike.com>
2020-03-14 13:39:20 -0700

    Initial Commit

---
+++ HuliPizza/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HuliPizza
+//
+//  Created by Caleb Rudnicki on 3/14/20.
+//  Copyright © 2020 Caleb Rudnicki. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
